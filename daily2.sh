dcae566a24d60d9a13bc87a0ae24da6298df467a
Simon Marwitz <simon.jakob.marwitz@uni-weimar.de>
2023-01-19 11:40:23 +0100

    update daily script

--- daily2.sh
+++ daily2.sh
@@ -3,13 +3,13 @@
 #rsync -a srv-grk:/home/towermonitoring/towerdata/ /vegas/scratch/womo1998/towerdata/towerdata_bin
 #module load python/intelpython3.7
 
+cd /home/towermonitoring/analysis/code/
+
 PATH=/vegas/apps/compiler/intel/intelpython3.7/bin:$PATH
 export PATH
 PYTHONPATH=/vegas/users/staff/womo1998/git/pyOMA
 export PYTHONPATH
-cd /home/towermonitoring/analysis/code/
 TMPDIR=/dev/shm/geyer_tmp
-
 
 rm -rf ${TMPDIR}
 mkdir ${TMPDIR}
@@ -18,7 +18,8 @@ DTSTART=`cat ${TMPDIR}/dtstart.tmp`
 python daily.py -d 60 -q temp --stats --tmp_dir=${TMPDIR} --dtstart=${DTSTART} >> ${TMPDIR}/geyer_out.txt
 python daily.py -d 30 -q temp --stats --tmp_dir=${TMPDIR} --dtstart=${DTSTART} >> ${TMPDIR}/geyer_out.txt
 python daily.py -d 10 -q temp  --stats --plot --tmp_dir=${TMPDIR} --dtstart=${DTSTART}  >> ${TMPDIR}/geyer_out.txt
-cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Temperature Records" -a ${TMPDIR}/*.png simon.jakob.marwitz@uni-weimar.de
+cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Temperature Records" -a ${TMPDIR}/stats_temp_10.png simon.jakob.marwitz@uni-weimar.de
+cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Temperature Records" -a ${TMPDIR}/stats_temp_10.png volkmar.zabel@uni-weimar.de
 
 rm -rf ${TMPDIR}
 mkdir ${TMPDIR}
@@ -27,7 +28,8 @@ DTSTART=`cat ${TMPDIR}/dtstart.tmp`
 python daily.py -d 60 -q wind --stats --tmp_dir=${TMPDIR} --dtstart=${DTSTART} >> ${TMPDIR}/geyer_out.txt
 python daily.py -d 30 -q wind --stats --tmp_dir=${TMPDIR} --dtstart=${DTSTART} >> ${TMPDIR}/geyer_out.txt
 python daily.py -d 10 -q wind  --stats --plot --tmp_dir=${TMPDIR} --dtstart=${DTSTART}  >> ${TMPDIR}/geyer_out.txt
-cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Wind Records" -a ${TMPDIR}/*.png simon.jakob.marwitz@uni-weimar.de
+cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Wind Records" -a ${TMPDIR}/stats_wind_10.png simon.jakob.marwitz@uni-weimar.de
+cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Wind Records" -a ${TMPDIR}/stats_wind_10.png volkmar.zabel@uni-weimar.de
 
 rm -rf ${TMPDIR}
 mkdir ${TMPDIR}
@@ -36,6 +38,7 @@ DTSTART=`cat ${TMPDIR}/dtstart.tmp`
 python daily.py -d 60 -q accel --stats --modal --tmp_dir=${TMPDIR} --dtstart=${DTSTART} >> ${TMPDIR}/geyer_out.txt
 python daily.py -d 30 -q accel --stats --modal --tmp_dir=${TMPDIR} --dtstart=${DTSTART} >> ${TMPDIR}/geyer_out.txt
 python daily.py -d 10 -q accel  --stats --plot --modal --tmp_dir=${TMPDIR} --dtstart=${DTSTART}  >> ${TMPDIR}/geyer_out.txt
-cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Acceleration Records" -a ${TMPDIR}/*.png simon.jakob.marwitz@uni-weimar.de
+cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Acceleration Records" -a ${TMPDIR}/stats_accel_10.png -a ${TMPDIR}/modal_accel_10.png -a ${TMPDIR}/spec_accel_10.png simon.jakob.marwitz@uni-weimar.de
+cat ${TMPDIR}/geyer_out.txt | mail -s "Structural Monitoring Tower: Daily Analysis of Acceleration Records" -a ${TMPDIR}/stats_accel_10.png -a ${TMPDIR}/modal_accel_10.png -a ${TMPDIR}/spec_accel_10.png volkmar.zabel@uni-weimar.de
 
-rm -rf /dev/shm/geyer_plots
+rm -rf ${TMPDIR}
